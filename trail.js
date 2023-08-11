2c2f2666f83752fe41e30505e581acd338172229
Rana <raselges127@gmail.com>
2023-08-11 13:46:07 +0600

    add funtion

--- trail.js
+++ trail.js
@@ -2,3 +2,14 @@ function myprofile(){
     console.log("rasel rana");
 }
 myprofile();
+
+const myObject = {
+    firstName:"John",
+    lastName: "Doe",
+    fullName: function() {
+      return this.firstName + " " + this.lastName;
+    }
+  }
+
+let mybj=myObject.fullName();
+console.log(mybj)
