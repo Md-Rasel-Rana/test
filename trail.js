41aa8f8879b33029345a48563e001c440211fbfe
Rana <raselges127@gmail.com>
2023-08-11 14:16:02 +0600

    addedt

--- trail.js
+++ trail.js
@@ -11,5 +11,19 @@ const myObject = {
     }
   }
 
-let mybj=myObject.fullName();
+let mybj= myObject.fullName();
 console.log(mybj)
+
+const person = {
+    fullName: function() {
+      return this.firstName + " " + this.lastName;
+    }
+  }
+  
+  const person1 = {
+    firstName: "Mary",
+    lastName: "Doe"
+  }
+  
+  // This will return "Mary Doe":
+  person.fullName.apply(person1);
