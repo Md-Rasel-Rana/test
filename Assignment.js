13e7ad799759562928982ac8f0749fc7b706c2e6
Rana <raselges127@gmail.com>
2023-08-13 20:20:48 +0600

    first commit

---
+++ Assignment.js
@@ -0,0 +1,162 @@
+        //Module-3 Assignment all question
+
+//1) javascript For in loop answer:
+function printObjectProperties() {
+  const person={
+    firstName:"John",
+     lastName:"Doe", 
+     age:50, 
+     eyeColor:"blue"
+  }
+  for( let prop in person){
+    console.log(prop + ": "+ person[prop]);
+  }
+
+};
+printObjectProperties();
+
+
+
+
+//2) javascript function returns answer:
+
+const numbers1 = [10, 20, 5, 30, 25];
+function findMaxNumber(numbers1) {
+  return Math.max.apply(" ", numbers1);
+}
+console.log(findMaxNumber(numbers1)); // Output: 30
+
+const numbers2 = [5, 15, 7];
+function findMaxNumber(numbers2) {
+  return Math.max.apply(" ", numbers2);
+}
+console.log(findMaxNumber(numbers2)); // Output: 15
+
+const emptyArray = [];
+function findMaxNumber(...emptyArray) {
+      if(emptyArray.length==0){
+            return null;
+      }
+  return Math.max(...emptyArray);
+  
+}
+console.log(findMaxNumber()); // Output: null
+
+
+
+
+ //3 javascript Spread Operator ans:
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+function mergeArrays(arr1,arr2){
+  return [...arr1,...arr2];
+}
+ console.log(mergeArrays(arr1,arr2)); // Output: [1, 2, 3, 4, 5, 6]
+
+
+
+
+
+// 4) javascript arrow function answer:
+ var calculateSquare=(x)=>{
+   return x*x;
+ }
+console.log(calculateSquare(5)); // Output: 25
+console.log(calculateSquare(8)); // Output: 64
+
+
+
+
+ //5) javascript es6 map
+
+            const numbers = [1, 2, 3, 4, 5];
+            const doubleNumbers = numbers.map((x) => x * 2);
+            console.log(doubleNumbers); 
+           // Output: [2, 4, 6, 8, 10]
+
+
+
+
+
+ //6) javascript es6 static keyword answer:
+      class MathUtility{
+
+        static multiply(x,y){
+            return x*y;
+        } 
+      }
+      console.log(MathUtility.multiply(5, 3)); // Output: 15
+      console.log(MathUtility.multiply(2, 8)); // Output: 16
+
+
+
+ //7) javascript es6 class inheritance answer:
+   class Animal{
+
+    constructor(name,sound){
+      this.name= name;
+      this.sound=sound;
+
+    }
+    makeSound(){
+      return this.sound;
+    }
+
+  }
+
+   class Dog extends Animal{
+    
+    fetch(){
+      console.log("Fetching the ball!");
+    }
+
+   }
+  const animal = new Animal("Lion", "Roar");
+  console.log(animal.makeSound()); // Output: "Roar"
+
+
+const dog = new Dog("Buddy", "Woof");
+console.log(dog.makeSound()); // Output: "Woof"
+dog.fetch(); // Output: "Fetching the ball!"
+
+
+
+      //8) javascript es6 super keyword answer:
+class Person{
+  constructor(name,age){
+    this.name = name;
+    this.age = age;
+  }
+  introduce(){
+    return `Hello, my name is ${this.name} and I am ${this.age} years old.`;
+  }
+}
+
+class Student extends Person {
+  constructor(name,age,major){
+    super(name,age)
+    this.major = major;
+  }
+  introduce1(){
+    super.introduce()
+    return `I am majoring in ${this.major}`;
+  
+   }
+}
+
+const person1 = new Person("John", 30);
+let result=person1.introduce(); // Output: "Hello, my name is John and I am 30 years old."
+console.log(result);
+
+
+const student1 = new Student("Alice", 20, "Computer Science");
+let result2= student1.introduce1(); // Output: "Hello, my name is Alice and I am 20 years old. I am majoring in Computer Science."
+console.log(result+result2);
+
+
+
+
+
+
+
+
--- test.js
+++ test.js
@@ -96,21 +96,249 @@
 // import {text} from "./es6.js"
 // console.log(text);
 
-class myclass{
- constructor(name,year){
-    this.name=name;
-    this.year=year;
- }
+// class myclass{
+//  constructor(name,year){
+//     this.name=name;
+//     this.year=year;
+//  }
 
- age(x){
-    return x = this.year;
- }
+//  age(x){
+//     return x = this.year;
+//  }
 
+// }
+// var obj =  new myclass("audi",2023);
+// obj.age();
+// console.log(obj);ler
+
+// let arrayy=["rana","tana","vana","fana","tola"];
+//  let [a,b,c,d,e]= arrayy;
+//  console.log(d);
+
+// class father{
+//    myfunction(){
+//       console.log("rasel rana her 1");
+//    }
+//    myfunction2(){
+//       console.log("rasel rana here 2");
+//    }
+   
+// }
+
+// class child extends father{
+//    myfunction2(){
+//       console.log("change the property here child class is fire now");
+//    }
+
+
+// }
+
+//  let obj= new child();
+//  obj.myfunction();
+//  obj.myfunction2();
+// class myclass {
+//    constructor(name,year){
+//       this.name=name;
+//       this.year=year;
+//    }
+//    age(){
+//       return `my name is ${this.name}`;
+//    }
+
+// }
+// let myobj = new myclass("jastin taudao",2023);
+// console.log(myobj);
+
+// let myfun = myobj.age();
+// console.log(myfun);
+
+// const Arr =[5, 10, 3, 7];
+//  let Outputt=Math.max(...Arr);
+//  console.log(Outputt); //Output 10
+
+//  const arr2=[2, 8, 4];
+//  let Outputt2=Math.max(...arr2);
+//  console.log(Outputt2); //Output 8
+
+// const person={
+//     name: "John",
+//      age: 30,
+//      occupation: "Engineer",
+// fullname:function(){
+//    return this.name + this.age + this.occupation ;
+   
+// }
+
+// };
+// // let result=new person;
+// // let finalresult=result.fullname;
+// // console.log(finalresult);
+// console.Log(person.fullName());
+
+// class myclass{
+//    constructor(width,height){
+//      this.width =width
+//      this.height=height;
+//    }
+//    fun(){
+//     return this.width * this.height;
+//    }
+// }
+// let myobj = new myclass(4,5);
+//  let x=myobj.fun();
+//  console.Log(x);
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const evenNumbers = getEvenNumbers(numbers);
+
+// console.log(evenNumbers); // Output: [2, 4, 6, 8, 10]
+// const numbers = [1, 2, 3, 4, 5];
+
+// const { first,second,third,fourth,last } = numbers;
+
+// console.log(first); // Output: 1
+
+// console.log(last); // Output: 5
+
+
+// function myFunction(x,y,z) {
+//    return x+y+z;
+//  }
+
+//  let numbers=[1,2,3,4,64,3,3,243,7,6,54,34,3,24,5,4,6,7];
+
+//  let num1 =[1,2,...numbers ,3,4,5,6,7,8,9];
+// //  let myout = myFunction(...numbers);
+//  console.log(num1);
+// const myObject={
+//   firstname:"john",
+//   lastname:"Doe",
+//   fullname:function(){
+//     return  this.firstname +" "+ this.lastname;
+//   }
+// }
+// let pp=myObject.fullname();
+// console.log(pp);
+
+
+// const person={
+//   fullName:function(){
+//    return this.firstname +" "+ this.lastname;
+//   }
+// }
+//  const person1={
+//   firstname:"john",
+//   lastname:"Doe"
+//  }
+// let re =person.fullName.call(person1);
+// console.log(re);
+// for(var i=5; i<=10; i++){
+//   console.log(i);
+// }
+// function myFunction(a,b) {
+//     console.log( a + b);
+// };
+
+// myFunction(4,3);
+// (function () {
+//    console.log("Hello! I called myself");
+// })();
+
+
+// function sum(...args) {
+//   let sum = 0;
+//   for (let value of args) sum += value;
+//   return sum;
+// }
+
+// let x = sum(4, 9, 16, 25, 29, 100, 66, 77);
+// console.log(x);
+
+// function myfunction(){
+//   let rana = 0;
+//   for ( var i=0; i<arguments.length; i++){
+//     rana+=arguments[i];
+
+//   }
+//   return rana;
+// }
+// var result = myfunction(1, 123, 500, 115, 44, 88);
+// console.log(result);
+// const myOb = {
+//   firstName:"rahim",
+//   lastName: "khan",
+//   fullName: function() {
+//     return this.firstName + " " + this.lastName;
+//   }
+// }
+// let rebj=myOb.fullName();
+// console.log(rebj);
+// const person = {
+
+//   fullName: function() {
+//     return this.firstName + " " + this.lastName;
+//   }
+// }
+// const person1 = {
+//   firstName:"jannat",
+//   lastName: "akter"
+// }
+// let reobj=person.fullName.apply(person1);
+// console.log(reobj)
+// // const person2 = {
+// //   firstName:"Maryyy",
+// //   lastName: "Doeee"
+// // }
+
+// // This will return "John Doe":
+// person.fullName.call(person1);
+
+// const person = {
+//   fullName: function(city, country) {
+//     return this.firstName + " " + this.lastName + "," + city + "," + country;
+//   }
+// }
+
+// const person1 = {
+//   firstName:"John",
+//   lastName: "Doe"
+// }
+// const person2 = {
+//   firstName:"Jannat",
+//   lastName: "akter"
+// }
+// let resultt = person.fullName.apply(person2,["Dhaka","india"]);
+// console.log(resultt);
+// class myclass{
+//   constructor(name,age){
+//     this.name=name;
+//     this.age=age;
+//   }
+
+// }
+// let myobj= new myclass("rasel",27);
+// console.log(myobj);
+// var num = true;
+// var num2="hello world";
+// console.log( num2);
+
+
+      //  //5) javascript es6 map
+      //     const numbers = [1, 2, 3, 4, 5];
+      //    const doubleNumbers = numbers.map((x) => x * 2);
+      //     console.log(doubleNumbers); 
+      //      // Output: [2, 4, 6, 8, 10]
+
+      const emptyArray = [];
+function findMaxNumber(...emptyArray) {
+      if(emptyArray.length==0){
+            return null;
+      }
+  return Math.max(...emptyArray);
+  
 }
-var obj =  new myclass("audi",2023);
-obj.age();
-console.log(obj);
-
+console.log(findMaxNumber()); // Output: null
+      
 
 
 
